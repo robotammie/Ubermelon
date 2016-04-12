d1e7d0ffb09c6e94f38b880cbc3453e89bf769c4
U-Lenovo\Tammie <tammie.kahnhauser@gmail.com>
2016-04-11 23:13:59 -0700

    cleaned up print function with nested for loops

--- accounting-scripts/melon-info.py
+++ accounting-scripts/melon-info.py
@@ -8,14 +8,13 @@ from melons import all_melons
 def print_melon(melons):
     """Print the name and attributes of melons on offer at Ubermelon"""
 
-    for name in melons:
+    for name, attributes in melons.items():
 
-        print "{}:".format(name)
-        print "Price: {}".format(melons[name]["price"])
-        print "Seedless?: {}".format(melons[name]["seedless"])
-        print "Rind Color: {}".format(melons[name]["rind color"])
-        print "Flesh Color: {}".format(melons[name]["flesh color"])
-        print "Weight: {} lbs\n".format(melons[name]["weight"])
+        print name
+
+        for index, data in attributes.items():
+            print "{}: {}".format(index, data)
+            print "\n"
 
 
 def add_attribute(attribute):
