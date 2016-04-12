8580f5a26a63f26b973772da7aa03498442b0d99
U-Lenovo\Tammie <tammie.kahnhauser@gmail.com>
2016-04-11 23:07:41 -0700

    Updated print function to use new data

--- accounting-scripts/melon-info.py
+++ accounting-scripts/melon-info.py
@@ -10,13 +10,12 @@ def print_melon(melons):
 
     for name in melons:
 
-        seeds = "have"
-        if melons[name]["seedless"]:
-            seeds = "do not have"
-
-        price = melons[name]["price"]
-
-        print "{}s {} seeds and are ${:.2f}".format(name, seeds, price)
+        print "{}:".format(name)
+        print "Price: {}".format(melons[name]["price"])
+        print "Seedless?: {}".format(melons[name]["seedless"])
+        print "Rind Color: {}".format(melons[name]["rind color"])
+        print "Flesh Color: {}".format(melons[name]["flesh color"])
+        print "Weight: {} lbs\n".format(melons[name]["weight"])
 
 
 def add_attribute(attribute):
