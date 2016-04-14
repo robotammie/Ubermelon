6f14227abe5af7a6d4f528ffb9565de67fce4f06
U-Lenovo\Tammie <tammie.kahnhauser@gmail.com>
2016-04-13 21:48:45 -0700

    Homework 13April

---
+++ melon-raffle/melon_raffle.py
@@ -0,0 +1,20 @@
+"""Randomly pick customer and print customer info"""
+
+from random import choice
+from customer_info import organize_customer_data
+
+
+def pick_winner(customers):
+    """Choose a random winner from list of customers."""
+
+    chosen_customer = choice(customers)
+
+    print "Contact {name} at {email} to notify them they've won".format(
+        name=chosen_customer.name,
+        email=chosen_customer.email
+        )
+
+
+customers = organize_customer_data("customers.txt")
+
+pick_winner(customers)
