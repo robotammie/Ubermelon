e76e037a6d4827229bc3ad2875be75f2f800b6c2
U-Lenovo\Tammie <tammie.kahnhauser@gmail.com>
2016-04-20 20:46:27 -0700

    Initial commit

---
+++ employee-directory-app/model.py
@@ -0,0 +1,99 @@
+employee_directory = {
+    "joel": {
+        "name": "Joel Burton",
+        "email": "joel@hackbrightacademy.com",
+        "favorite_food": "peanut-butter pretzels",
+        "position": "Vice President of Education"
+    },
+    "rachel": {
+        "name": "Rachel Thomas",
+        "email": "rthomas@hackbrightacademy.com",
+        "favorite_food": "dried mango",
+        "position": "Senior Instructor"
+    },
+    "henry": {
+        "name": "Henry Chen",
+        "email": "henry@hackbrightacademy.com",
+        "favorite_food": "Chinese donuts",
+        "position": "Instructor"
+    },
+    "meggie": {
+        "name": "Meggie Mahnken",
+        "email": "meggie@hackbrightacademy.com",
+        "favorite_food": "Foods with single, overpowering flavors",
+        "position": "Instructor"
+    },
+    "bonnie": {
+        "name": "Bonnie Schulkin",
+        "email": "bonnie@hackbrightacademy.com",
+        "favorite_food": "chocolate cake with raspberries",
+        "position": "Instructor"
+    },
+    "heather": {
+        "name": "Heather Bryant",
+        "email": "heather@hackbrightacademy.com",
+        "favorite_food": "Jell-O",
+        "position": "Lab Instructor"
+    },
+    "kara": {
+        "name": "Kara DeWalt",
+        "email": "kara@hackbrightacademy.com",
+        "favorite_food": "gummy bears",
+        "position": "Teaching Assistant"
+    },
+    "anli": {
+        "name": "Anli Yang",
+        "email": "anli@hackbrightacademy.com",
+        "favorite_food": "ice cream",
+        "position": "Teaching Assistant"
+    },
+    "jessica": {
+        "name": "Jessica Earley",
+        "email": "jessica@hackbrightacademy.com",
+        "favorite_food": "strawberries",
+        "position": "Teaching Assistant"
+    },
+    "lavinia": {
+        "name": "Lavinia Karl",
+        "email": "lavinia@hackbrightacademy.com",
+        "favorite_food": "chocolate",
+        "position": "Lab Instructor"
+    },
+    "meg": {
+        "name": "Meg Bishop",
+        "email": "meg@hackbrightacademy.com",
+        "favorite_food": "waffles",
+        "position": "Teaching Assistant"
+    },
+    "katie": {
+        "name": "Katie Byers",
+        "email": "katiebyers@hackbrightacademy.com",
+        "favorite_food": "sushi",
+        "position": "Teaching Assistant"
+    },
+    "leslie": {
+        "name": "Leslie Castellanos",
+        "email": "leslie@hackbrightacademy.com",
+        "favorite_food": "dark chocolate",
+        "position": "Teaching Assistant"
+    },
+    "allyson": {
+        "name": "Allyson McKnight",
+        "email": "ally@hackbrightacademy.com",
+        "favorite_food": "jamon iberico",
+        "position": "Teaching Assistant"
+    },
+    "balloonicorn": {
+        "name": "Balloonicorn",
+        "email": "balloonicorn@hackbrightacademy.com",
+        "favorite_food": "magical rainbow cupcakes with iridescent sprinkles",
+        "position": "Support Staff"
+    },
+
+    "mel": {
+        "name": "Mel",
+        "email": "mel@ubermelon.com",
+        "favorite_food": "Powerbars",
+        "position": "The Boss"
+    }
+}
